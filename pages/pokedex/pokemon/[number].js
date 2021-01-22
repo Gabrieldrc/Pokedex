479c0674a17eab3df5b37c9e6e637e15cae5a1b6
Gabrieldrc <gabriel.drc25@gmail.com>
2021-01-22 20:23:46 -0300

    Fetch pokemon data

--- pages/pokedex/pokemon/[number].js
+++ pages/pokedex/pokemon/[number].js
@@ -1,12 +1,35 @@
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/router'
+import { capitalize } from '../../../lib/functions'
+import { getPokemon } from '../../../lib/pokedex.api'
 
-import { getPokemon } from '../../../lib/pokedex.api';
-
-export default function PokemonNumber() {
-  const { number } = useRouter().query;
+function PokemonNumber({ pokemonData }) {
+  const router = useRouter();
+  const { number } = router.query;
+  if (router.isFallback) {
+    return <>...</>
+  }
+  console.log(pokemonData)
   return(
     <>
-      {number}
+      {capitalize(pokemonData.name)}
     </>
   );
 }
+
+export async function getStaticPaths() {
+  return { paths: [], fallback: true, }
+}
+
+// This also gets called at build time
+export async function getStaticProps({ params }) {
+  const { number } = params
+  const pokemonData = await getPokemon(parseInt(number))
+
+  return {
+    props: { 
+      pokemonData,
+    }
+  }
+}
+
+export default PokemonNumber
